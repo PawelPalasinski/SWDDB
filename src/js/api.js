a57b7cc45838110740ee4847c15c46607846f611
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-04-19 01:14:33 +0200

    axios

--- src/js/api.js
+++ src/js/api.js
@@ -1,10 +1,27 @@
-async function fetchSWDDB() {
-  const response = await fetch(`https://swdestinydb.com/api/public/cards/`);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch data: ${response.status}`);
+// async function fetchSWDDB() {
+//   const response = await fetch(`https://swdestinydb.com/api/public/cards/`);
+//   if (!response.ok) {
+//     throw new Error(`Failed to fetch data: ${response.status}`);
+//   }
+//   const data = await response.json();
+//   return data;
+// }
+
+// export default fetchSWDDB;
+
+import axios from "axios";
+
+const fetchSWDDB = async () => {
+  try {
+    const response = await axios.get(
+      `https://swdestinydb.com/api/public/cards/`
+    );
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(`Error fetching data:`, error.message);
+    return undefined;
   }
-  const data = await response.json();
-  return data;
-}
+};
 
 export default fetchSWDDB;
