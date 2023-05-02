c2db413513e1b531cf65af85fa819cd55e41b816
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-05-03 01:52:15 +0200

    statistics as background to Cards

--- src/components/cardCounter/CardCounter.jsx
+++ src/components/cardCounter/CardCounter.jsx
@@ -1,6 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
 import useCardCountStore from "../../store/cardCountStore";
 import Statistics from "../statistics/Statistics";
+
+const CardAmount = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20% 0 0 0;
+  padding: 0;
+  list-style: none;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, 50%);
+
+  li {
+    margin-right: 10px;
+    font-size: 16px;
+    font-weight: bold;
+  }
+`;
 
 function CardCounter() {
   const cardCount = useCardCountStore((state) => state.cardCount);
@@ -11,11 +32,13 @@ function CardCounter() {
 
   return (
     <>
-      <div>Cards {cardCount}</div>
-      <div>Red {cardRedCount}</div>
-      <div>Yellow {cardYellowCount}</div>
-      <div>Blue {cardBlueCount}</div>
-      <div>Gray {cardGrayCount}</div>
+      <CardAmount>
+        <li>All: {cardCount} ✴</li>
+        <li>Red: {cardRedCount} ✴</li>
+        <li>Yellow: {cardYellowCount} ✴</li>
+        <li>Blue: {cardBlueCount} ✴</li>
+        <li>Gray: {cardGrayCount}</li>
+      </CardAmount>
       <Statistics />
     </>
   );
--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
 import AllCards from "../components/cards/AllCards";
 import FactionChangeOptions from "../components/filters/FactionChangeOptions";
 import RarityChangeOptions from "../components/filters/RarityChangeOptions";
 import ExpansionChangeOptions from "../components/filters/ExpansionChangeOptions";
 import SearchByName from "../components/filters/SearchByName";
 import CardCounter from "../components/cardCounter/CardCounter";
+
+const CardCounterContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: -1; /* przesuwamy element na dalszy plan, aby nie przysłaniał reszty treści */
+`;
 
 import useCollectionStore from "../store/collectionStore";
 
@@ -27,9 +41,9 @@ function Cards() {
 
   return (
     <div>
-      <div>
+      <CardCounterContainer>
         <CardCounter />
-      </div>
+      </CardCounterContainer>
 
       <div>
         <SearchByName />
