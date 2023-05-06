44d8f726497351149c96a0845410ddaaa678d3f6
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-05-06 17:43:22 +0200

    working contact form

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,84 +1,38 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
-import emailjs from "emailjs-com";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-  const { register, handleSubmit, errors } = useForm();
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const form = useRef();
 
-  //   const sendEmail = (e) => {
-  //     e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  //     emailjs
-  //       .sendForm(
-  //         process.env.REACT_APP_EMAILJS_SERVICE_ID,
-  //         process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-  //         e.target,
-  //         process.env.REACT_APP_EMAILJS_USER_ID
-  //       )
-  //       .then(
-  //         (result) => {
-  //           console.log(result.text);
-  //         },
-  //         (error) => {
-  //           console.log(error.text);
-  //         }
-  //       );
-  //   };
-
-  const sendEmail = (data) => {
     emailjs
-      .send(
-        import.meta.env.VITE_REACT_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_REACT_APP_EMAILJS_TEMPLATE_ID,
-        data,
-        import.meta.env.VITE_REACT_APP_EMAILJS_USER_ID
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         (result) => {
-          console.log(result.text);
+          console.log("SUCCESS!", result.text);
         },
         (error) => {
-          console.log(error.text);
+          console.log("FAILED...", error.text);
         }
       );
   };
 
-  const onSubmit = (data) => {
-    setIsSubmitted(true);
-    sendEmail(data);
-  };
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="name">Imię i nazwisko:</label>
-      <input
-        id="name"
-        name="name"
-        type="text"
-        {...register("name", { required: true })}
-      />
-      {errors && <span>To pole jest wymagane</span>}
-
-      <label htmlFor="email">Adres e-mail:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        {...register("email", { required: true })}
-      />
-      {errors && <span>To pole jest wymagane</span>}
-
-      <label htmlFor="message">Wiadomość:</label>
-      <textarea
-        id="message"
-        name="message"
-        {...register("message", { required: true })}
-      ></textarea>
-      {errors && <span>To pole jest wymagane</span>}
-
-      <button type="submit">Wyślij</button>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
     </form>
   );
 };
