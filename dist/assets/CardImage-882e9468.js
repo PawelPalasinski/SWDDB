b72a8ff89b31a00526325741a0311fe92957382f
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-05-14 01:36:46 +0200

    Adding dist

---
+++ dist/assets/CardImage-882e9468.js
@@ -0,0 +1,5 @@
+import{s as t,j as a}from"./index-f2ff656e.js";const s=t.img`
+  max-height: 200px;
+  object-fit: cover;
+  border-radius: 8px;
+`,d=({src:e,alt:r})=>a(s,{src:e,alt:r});export{d as default};
