c7aa26c88dc18d7f9a47dc2b48cdb65fe8f31292
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-05-15 23:54:20 +0200

    Home - paralax - work in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+import backgroundImage from "../assets/images/deathstarwallpaper.jpg";
+import layer from "../assets/images/layer1.png";
+import logo from "../assets/images/logo.png";
+
+const ParallaxContainer = styled.div`
+  position: relative;
+  height: 400px; /* Ustaw wysokość, która pasuje do Twoich potrzeb */
+  overflow: hidden;
+`;
+
+const ParallaxText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: #fff;
+`;
+
+const ParallaxImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  height: 100%;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+  opacity: 0.5; /* Ustaw poziom przezroczystości, który Ci odpowiada */
+  z-index: -1;
+`;
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <ParallaxContainer>
+      <ParallaxText>
+        <p>Welcome to the Star Wars Destiny card collection app</p>
+      </ParallaxText>
+      <ParallaxImage />
+    </ParallaxContainer>
+  );
 }
 
 export default Home;
