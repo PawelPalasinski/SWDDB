6583a10d1409a187b5e86cfb9fd1ad1b78f5b63b
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-05-19 22:21:16 +0200

    Updates

--- assets/CardImage-573dd744.js
+++ assets/CardImage-573dd744.js
@@ -1,4 +1,4 @@
-import{s as t,j as a}from"./index-ccb39718.js";const s=t.img`
+import{s as t,j as a}from"./index-95ef9962.js";const s=t.img`
   max-height: 200px;
   object-fit: cover;
   border-radius: 8px;
