da11834b90a94dc8a8040fe6e5d35513847051f6
PawelPalasinski <PawelPalasinski@users.noreply.github.com>
2023-06-02 18:47:38 +0000

    deploy: 7ba5f35311d4d2cf660332659222f618f1867b0b

--- assets/CardImage-ccc19471.js
+++ assets/CardImage-ccc19471.js
@@ -1,4 +1,4 @@
-import{s as t,j as a}from"./index-d36bd877.js";const s=t.img`
+import{s as t,j as a}from"./index-1ab670e9.js";const s=t.img`
   max-height: 200px;
   object-fit: cover;
   border-radius: 8px;
