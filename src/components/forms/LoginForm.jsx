7ba5f35311d4d2cf660332659222f618f1867b0b
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-06-02 20:46:39 +0200

    login and register basic styles

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,6 +1,56 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../../store/authStore";
+import styled from "styled-components";
+
+const LoginPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LoginTitle = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+  margin-bottom: 10px;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InputField = styled.input`
+  width: 300px;
+  height: 40px;
+  padding: 8px;
+  margin-bottom: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const LoginButton = styled.button`
+  width: 200px;
+  height: 40px;
+  background-color: #ffd700;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #f0c809;
+  }
+`;
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -18,23 +68,25 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      {errorMessage && <p>{errorMessage}</p>}
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <LoginPageWrapper>
+      <LoginTitle>Login</LoginTitle>
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+      <LoginForm>
+        <InputField
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <InputField
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <LoginButton onClick={handleLogin}>Login</LoginButton>
+      </LoginForm>
+    </LoginPageWrapper>
   );
 };
 
--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,7 +1,56 @@
-// RegisterPage.jsx
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../../store/authStore";
+import styled from "styled-components";
+
+const RegisterPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const RegisterTitle = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
+  margin-bottom: 10px;
+`;
+
+const RegisterForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InputField = styled.input`
+  width: 300px;
+  height: 40px;
+  padding: 8px;
+  margin-bottom: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const RegisterButton = styled.button`
+  width: 200px;
+  height: 40px;
+  background-color: #ffd700;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #f0c809;
+  }
+`;
 
 const RegisterPage = () => {
   const [username, setUsername] = useState("");
@@ -23,23 +72,25 @@ const RegisterPage = () => {
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      {errorMessage && <p>{errorMessage}</p>}
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleRegistration}>Register</button>
-    </div>
+    <RegisterPageWrapper>
+      <RegisterTitle>Register</RegisterTitle>
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+      <RegisterForm>
+        <InputField
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <InputField
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <RegisterButton onClick={handleRegistration}>Register</RegisterButton>
+      </RegisterForm>
+    </RegisterPageWrapper>
   );
 };
 
