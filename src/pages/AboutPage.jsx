3d2ff398fca722eb0709044bc7b0d223b82cc647
Paweł Pałasiński <pawel.palasinski@gmail.com>
2023-06-09 02:10:38 +0200

    amendments

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-// import EmptyCollection from "../components/emptyCollection/EmptyCollection";
-import CollectionCopy from "../components/collection/CollectionCopy";
 
 const AboutPage = () => {
   return (
     <>
       <div>About</div>;
-      <CollectionCopy />
     </>
   );
 };
