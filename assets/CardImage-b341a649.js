2a39291d75aadafc68713551abd2a35e9a36e7e8
PawelPalasinski <PawelPalasinski@users.noreply.github.com>
2023-06-10 21:59:14 +0000

    deploy: 262d68fd219883d29d9afb3cb7c4ff245d7785c7

--- assets/CardImage-b341a649.js
+++ assets/CardImage-b341a649.js
@@ -1,4 +1,4 @@
-import{s as t,j as a}from"./index-b4fb347a.js";const s=t.img`
+import{s as t,j as a}from"./index-5e691e84.js";const s=t.img`
   max-height: 200px;
   object-fit: cover;
   border-radius: 8px;
