3fb44121de9af735584840461ffccba7ee14032c
PawelPalasinski <PawelPalasinski@users.noreply.github.com>
2023-07-04 23:07:17 +0000

    deploy: b324eac6827966a62a1a8af72c28502157f5f994

--- assets/CardImage-be0ff335.js
+++
@@ -1,5 +0,0 @@
-import{s as t,j as a}from"./index-6e2090f9.js";const s=t.img`
-  max-height: 200px;
-  object-fit: cover;
-  border-radius: 8px;
-`,d=({src:e,alt:r})=>a(s,{src:e,alt:r});export{d as default};
